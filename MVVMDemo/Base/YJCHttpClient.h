a1fbf054f8e530764b3c0dccb394f2ccde6443bf
chun <chun@chundeMacBook-Pro.local>
2017-07-28 17:55:11 +0800

    add httpclient

---
+++ MVVMDemo/Base/YJCHttpClient.h
@@ -0,0 +1,13 @@
+//
+//  YJCHttpClient.h
+//  MVVMDemo
+//
+//  Created by chun on 2017/7/28.
+//  Copyright © 2017年 chun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJCHttpClient : NSObject
+
+@end
---
+++ MVVMDemo/Base/YJCHttpClient.m
@@ -0,0 +1,13 @@
+//
+//  YJCHttpClient.m
+//  MVVMDemo
+//
+//  Created by chun on 2017/7/28.
+//  Copyright © 2017年 chun. All rights reserved.
+//
+
+#import "YJCHttpClient.h"
+
+@implementation YJCHttpClient
+
+@end
