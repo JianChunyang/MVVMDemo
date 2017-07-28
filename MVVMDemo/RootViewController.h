e3fe6b0be90c36f815b5e6d3fe3a02590578c140
chun <chun@chundeMacBook-Pro.local>
2017-07-28 17:45:08 +0800

    init

---
+++ MVVMDemo/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  MVVMDemo
+//
+//  Created by chun on 2017/7/28.
+//  Copyright © 2017年 chun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+
+@end
+
---
+++ MVVMDemo/RootViewController.m
@@ -0,0 +1,30 @@
+//
+//  RootViewController.m
+//  MVVMDemo
+//
+//  Created by chun on 2017/7/28.
+//  Copyright © 2017年 chun. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"root viewController";
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
